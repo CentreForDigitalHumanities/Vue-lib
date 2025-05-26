18446065ed1f617f528b2409a359e4c8c92551d5
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2025-05-26 11:48:21 +0200

    Add Bootstrap JS to app

--- vue-lib-doc/nuxt.config.ts
+++ vue-lib-doc/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            script: [
+                {
+                    // Bootstrap JS is needed for BSDropdown.
+                    src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+                    defer: true,
+                }
+            ]
+        }
+    },
     // Enabling SSR breaks Bootstrap JS, which expects document.
     ssr: false,
     compatibilityDate: "2024-11-01",
